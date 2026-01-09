27f2ea874d5a7240822d45a404fc834f8e29a060
Nycole Silva <nycole.xavierr@gmail.com>
2026-01-09 09:26:04 -0300

    feat: mudando /login para vuetify

--- src/pages/login/Login.vue
+++ src/pages/login/Login.vue
@@ -1,21 +1,52 @@
-<script lang="ts"></script>
+<script lang="ts">
+import { ROUTES } from '@/router/utils/routes';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'LoginPage',
+
+  methods: {
+    irParaODashboard() {
+      this.$router.push(ROUTES.dashboard);
+    },
+  },
+});
+</script>
 
 <template>
-  <div>
-    <h1>Tela de login</h1>
+  <v-container class="fill-height" fluid>
+    <v-row align="center" justify="center">
+      <v-col cols="12" sm="8" md="4">
+        <v-card variant="outlined">
+          <v-card-title class="text-center">
+            <h2>Login</h2>
+          </v-card-title>
 
-    <div>
-      email
+          <v-card-text>
+            <v-form>
+              <v-text-field
+                label="E-mail"
+                type="email"
+                variant="outlined"
+                placeholder="Digite seu e-mail"
+              />
 
-      <input type="text" />
-    </div>
+              <v-text-field
+                label="Senha"
+                type="password"
+                variant="outlined"
+                placeholder="Digite sua senha"
+              />
+            </v-form>
+          </v-card-text>
 
-    <div>
-      senha
-
-      <input type="email" name="" id="" label="Digite aqui sua senha" />
-    </div>
-
-    <v-btn>login</v-btn>
-  </div>
+          <v-card-actions>
+            <v-btn @click="irParaODashboard" block color="primary">
+              Entrar
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
